46d9c5430fb0ff5c85b4fc8698adb42df083c72a
Omnishade <shawnrobinson1081@gmail.com>
2020-10-09 00:47:44 -0700

    include extra files from source to deploy backend

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -35,8 +35,8 @@ export class CdkStack extends Cdk.Stack {
           },
         },
         artifacts: {
-          'base-directory': 'cdk/cdk.out',
-          files: ['**/*'],
+          'base-directory': 'cdk',
+          files: ['cdk.out/**/*', 'bin/*', 'package.json'],
         },
       }),
       environment: {
